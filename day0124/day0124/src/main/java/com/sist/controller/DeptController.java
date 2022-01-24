9bd7d52f8a0cf107fcff553c480f4876c3ff464b
pp1212 <pagy1218@hanmail.net>
2022-01-24 22:20:05 +0900

    ajax 이용

---
+++ day0124/day0124/src/main/java/com/sist/controller/DeptController.java
@@ -0,0 +1,33 @@
+package com.sist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
+import com.sist.dao.DeptDao;
+
+@Controller
+public class DeptController {
+	
+	@Autowired
+	private DeptDao dao;
+
+	public void setDao(DeptDao dao) {
+		this.dao = dao;
+	}
+	
+	@RequestMapping(value = "/listDept.do",produces = "text/plain;charset=utf-8")
+	@ResponseBody	//json을 요청할 때
+	public String list() {
+		Gson gson = new Gson();
+		return gson.toJson(dao.findAll());
+	}
+	
+	@RequestMapping("/deptTest.do")
+	public void deptTest() {
+		
+	}
+}
