b92eb539ecf4d7917e5c692a80fda4278435c9f8
pp1212 <pagy1218@hanmail.net>
2022-02-16 18:40:34 +0900

    restful 어플리케이션,아임포트-결제

---
+++ day0216/restful_test/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.CustomerDAO;
+import com.example.demo.vo.CustomerVO;
+
+import lombok.Setter;
+
+@RestController
+@Setter
+public class CustomerController {
+	@Autowired
+	private CustomerDAO dao;
+	
+	@RequestMapping("/listCustomer")	//json으로 응답이 됨
+	public List<CustomerVO> listCustomer(){
+		return dao.findAll();
+	}
+	
+	@RequestMapping("/detailCustomer/{custid}")
+	public CustomerVO findByCustId(@PathVariable int custid) {
+		return dao.findByCustId(custid);
+	}
+}
---
+++ day0216/restful_test/src/main/java/com/example/demo/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.PaymentDAO;
+import com.example.demo.vo.PaymentVO;
+
+import lombok.Setter;
+
+@RestController
+@Setter
+public class PaymentController {
+	
+	@Autowired
+	private PaymentDAO dao;
+	
+	@RequestMapping("/insertPayment")
+	public String insertPayment(PaymentVO p) {
+		return dao.insert(p)+"";
+	}
+}
